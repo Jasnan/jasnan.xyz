57c0c9a1c83c7c29898535f5eb376657b00d46f9
Jasnan Thachaparamban <hello@jasnan.xyz>
2021-07-23 00:45:26 +0200

    initial setup

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,9 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What is you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Genie Multi-Search',
+    description: `Genie Multi-Search is a free and open-source chrome browser extension. It helps language learners to do faster word research.`,
+    imgSrc: '/static/images/avatar.png',
+    href: 'https://goo.gl/rGOI7R',
   },
 ]
 
